8f5f134e28f6411f5c8c5c68980eeb96cfa128fd
shafiq <shafiqahmad081@gmail.com>
2024-06-03 22:35:54 +0600

    complete header section and responsive design

---
+++ src/assets/js/main.js
@@ -0,0 +1,35 @@
+const hamburgerBtn = document.getElementById("hamberger");
+const closeBtn = document.getElementById("close");
+const menuNav = document.getElementById("menu");
+const mainDiv = document.getElementById("main");
+const snapscanLogoWhite = document.getElementById("snapscan-white");
+const snapscanLogo = document.getElementById("snapscan");
+
+function show() {
+  menuNav.classList.remove("hidden");
+  menuNav.classList.add("block");
+  mainDiv.classList.add("nav-expanded");
+  closeBtn.classList.remove("hidden");
+  closeBtn.classList.add("block");
+  hamburgerBtn.classList.add("hidden");
+  hamburgerBtn.classList.remove("block");
+  snapscanLogoWhite.classList.add("block");
+  snapscanLogoWhite.classList.remove("hidden");
+  snapscanLogo.classList.add("nav-expanded");
+}
+
+function close() {
+  menuNav.classList.remove("block");
+  menuNav.classList.add("hidden");
+  closeBtn.classList.remove("block");
+  closeBtn.classList.add("hidden");
+  hamburgerBtn.classList.add("block");
+  hamburgerBtn.classList.remove("hidden");
+  mainDiv.classList.remove("nav-expanded");
+  snapscanLogoWhite.classList.add("hidden");
+  snapscanLogoWhite.classList.remove("block");
+  snapscanLogo.classList.remove("nav-expanded");
+}
+
+hamburgerBtn.addEventListener("click", show);
+closeBtn.addEventListener("click", close);
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        proxima: ['"Proxima Nova", sans-serif'],
+      },
+      colors: {
+        "sky-1": "rgba(187, 216, 249, 0.5)",
+        "black-1": "rgb(20, 54, 65)",
+        "blue-1": "#4a90e2",
+        "gray-1": "#9faab4",
+      },
+      fontSize: {
+        "2.1xl": "1.625rem",
+      },
+    },
   },
   plugins: [],
 };
