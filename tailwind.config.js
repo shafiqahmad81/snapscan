29926a1c92690a95984df43812b6805b32126e54
shafiq <shafiqahmad081@gmail.com>
2024-06-11 20:51:57 +0600

    complete get started section

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,22 +9,29 @@ module.exports = {
       },
       fontFamily: {
         proxima: ['"Proxima Nova", sans-serif'],
+        Lora: ['"Lora", sans-serif'],
       },
       colors: {
         "sky-1": "rgba(187, 216, 249, 0.5)",
         "black-1": "rgb(20, 54, 65)",
+        "black-2": "rgb(38, 57, 67)",
         "black-1.1": "#263943",
         "blue-1": "#4a90e2",
         "gray-1": "#9faab4",
         "gray-1.1": "#e3ebf5",
         "gray-1.2": "#f8fafd",
         "gray-1.9": "#5a707b",
+        "gray-2": "#eff5f9",
       },
       fontSize: {
         "2.1xl": "1.625rem",
       },
       boxShadow: {
-        "3xl": "0 4px 10px 0 rgba(200, 216, 235, 0.8)",
+        "3xl": "0 15px 20px 0 rgba(59, 119, 187, 0.2)",
+      },
+      maxWidth: {
+        "1.5lg": "33.75rem",
+        "6.5xl": "76.875rem",
       },
     },
   },
